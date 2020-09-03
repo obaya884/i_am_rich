77c0289e88c4384b11a6338dab1c523dbf5987f0
obaya884 <taku3.884@gmail.com>
2020-09-03 13:18:11 +0900

    try using Scaffold class

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,21 @@ import 'package:flutter/material.dart';
 
 // The main function is the starting point for our all our Flutter apps.
 void main() {
-  runApp(MaterialApp(
-    home: Center(
-      child: Text('Hello, World'),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I am Rich.'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg'),
+          ),
+        ),
+      ),
     ),
-  ));
+  );
 }
